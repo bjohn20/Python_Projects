38cdcf5fb3bf37b245674b0f35c1b8aa0a1298d2
Brandon-J <johnson.c.brandon@gmail.com>
2024-01-19 14:17:03 -0500

    Added app that detects modified files within 24 hours and trasfers to destination folder

---
+++ file_transfer.py
@@ -0,0 +1,113 @@
+
+import tkinter as tk
+from tkinter import *
+import tkinter.filedialog
+import os
+import shutil
+import datetime
+from datetime import timedelta
+import time
+
+
+class ParentWindow(Frame):
+    def __init__(self,master):
+        Frame.__init__(self)
+        # Sets title of GUI window
+        self.master.title("File Transfer")
+
+        # Creates button to select files from source directory
+        self.sourceDir_btn = Button(text="Select Source", width=20, command=self.sourceDir)
+        # Positions source button in GUI using tkinter grid()
+        self.sourceDir_btn.grid(row=0, column=0, padx=(30, 10), pady=(30, 0))
+
+        # Creates entry for source directory selection
+        self.source_dir = Entry(width=75)
+        # Positions entry in GUI using tkinter grid() padx andpady are the same as
+        # the button to ensure they will line up
+        self.source_dir.grid(row=0, column=1, columnspan=2, padx=(20, 10), pady=(30, 0))
+
+        # Creates button to select destination of files from destination directory
+        self.destDir_btn = Button(text="Select Destination", width=20, command=self.destDir)
+        # Positions destination button in GUI using tkinter grid()
+        # on the next row under the source button
+        self.destDir_btn.grid(row=1, column=0, padx=(20, 10), pady=(15, 10))
+
+        # Creates button to transfer files
+        self.transfer_btn = Button(text="Transfer Files", width=20, command=self.transferFiles)
+        # Positions transfer files button
+        self.transfer_btn.grid(row=2, column=1, padx=(200, 0), pady=(0, 15))
+
+        # Creates entry for destination directory selection
+        self.destination_dir = Entry(width=75)
+        # Positions entry in GUI using tkinter grid() padx and pady are the same as
+        # the button to ensure they will line up
+        self.destination_dir.grid(row=1, column=1, columnspan=2, padx=(20, 10), pady=(15, 10))
+
+        # Creates an exit button
+        self.exit_btn = Button(text="Exit", width=20, command=self.exit_program)
+        # Positions the exit button
+        self.exit_btn.grid(row=2, column=2, padx=(10, 40), pady=(0, 15))
+        
+    def sourceDir(self):
+        selectSourceDir = tkinter.filedialog.askdirectory()
+        # The .delete(0, END) will clear the content that is inserted in the Entry widget,
+        # this allows the path to be inserted into the Entry widget properly
+        self.source_dir.delete(0, END)
+        # The .insert method will insert the user selection to the source_dir Entry
+        self.source_dir.insert(0, selectSourceDir)
+
+    # Creates function to select destination directory
+    def destDir(self):
+        selectDestDir = tkinter.filedialog.askdirectory()
+        # The .delete(0, END) will clear the content that is inserted in the Entry widget
+        # this allows the path to be inserted into the Entry widget properly
+        self.destination_dir.delete(0, END)
+        # The .insert method will insert the user selection to the destination_dir Entry widget
+        self.destination_dir.insert(0, selectDestDir)
+
+    def transferFiles(self):
+        # Gets source directory
+        source = self.source_dir.get()
+        # Gets destination dorectory
+        destination = self.destination_dir.get()
+        # Gets a list of files in the source directory
+        source_files = os.listdir(source)
+        # Gets the time of last modification of the specified path since the epoch
+        modification_time = os.path.getmtime(source)
+        # converts the time from seconds since epoch to local time
+        local_time = time.ctime(modification_time)
+        # Get the exact current moment in time right now
+        current_time = datetime.datetime.now()
+        # Converting modification_time to a datetime object
+        mod_modificaton_time = datetime.datetime.fromtimestamp(modification_time)
+        # subtracting the modification time from the current time
+        time_diff = current_time - mod_modificaton_time
+        # Printing time diff
+        print(time_diff)
+        # Get 24 hours datetime object
+        twenty_four_hours = timedelta(days=1)
+        # Checking if results of that deduction is less than 24 hours
+        if time_diff < twenty_four_hours:
+            # Runs through each file in the source directory
+            for i in source_files:
+                # moves each file from the source to the destination
+                shutil.move(source + '/' + i, destination)
+                print(i + ' was successfully transferred')
+                print("Last modification time:", local_time)
+            
+
+        
+
+    def exit_program(self):
+        # root is the main GUI window, the Tkinter destroy method
+        # tells python to terminate root.mainloop and all widgets inside the GUI window
+        root.destroy()
+
+    
+    
+
+
+if __name__ == "__main__":
+    root = tk.Tk()
+    App = ParentWindow(root)
+    root.mainloop()
